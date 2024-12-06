1add0dd66d159babff89a28f53af86fed4e0b9a8
Bernd Konrad <bkonrad@derdummepunkt.de>
2024-12-06 22:39:24 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # redmine_docker_compose_sidekiq
+
+This repo is meant to showcase proposed changes to https://github.com/docker-library/redmine/ as well as provide an example on how to use the proposed changes in a docker compose context.
+
+The compose stack runs the same image as two services:
+- one running the ruby on rails redmine application
+- the other one running the sidekiq background worker
+
+Additionally there are services for
+- postgres as a database
+- valkey as a redis-like kv-store
+
+**This setup assumes you are running a reverse proxy for http/https and is not part of main compose stack**. There is `proxy/docker-compose.yml` however, that shows how that is intended to work.
+
+## Credentials and secrets
+
+To configure credentials for the database and key-value store, this repo utilizes docker secrets.
+
+Place the desired passwords in text files with the corresponding names in `./secrets`, e.g. via:
+```bash
+openssl rand -hex 64 >> ./secrets/DB_PASSWORD
+openssl rand -hex 64 >> ./secrets/REDIS_PASSWORD
+```
+The values are mapped and used in the respective services accordingly.
+
+## run the stack
+
+- create the external network
+    ```bash
+    docker network create web
+    ```
+- run the stack
+    ```bash
+    docker compose up -d
+    ```
+- watch the logs
+    ```bash
+    docker compose logs -f --tail=100
+    ```
+- eventually, you should see output like
+    ```bash
+    sidekiq-1  | 2024-12-06T20:52:31.407Z pid=1 tid=41l INFO: Sidekiq 7.3.6 connecting to Redis with options {:size=>10, :pool_name=>"internal", :url=>"redis://:REDACTED@valkey:6379/0"}
+    sidekiq-1  | 2024-12-06T20:52:31.410Z pid=1 tid=41l INFO: Sidekiq 7.3.6 connecting to Redis with options {:size=>5, :pool_name=>"default", :url=>"redis://:REDACTED@valkey:6379/0"}
+    redmine    | => Booting Puma
+    redmine    | => Rails 7.2.2 application starting in production
+    redmine    | => Run `bin/rails server --help` for more startup options
+    redmine    | Puma starting in single mode...
+    redmine    | * Puma version: 6.5.0 ("Sky's Version")
+    redmine    | * Ruby version: ruby 3.3.6 (2024-11-05 revision 75015d4c1f) [x86_64-linux]
+    redmine    | *  Min threads: 0
+    redmine    | *  Max threads: 5
+    redmine    | *  Environment: production
+    redmine    | *          PID: 1
+    redmine    | * Listening on http://0.0.0.0:3000
+    ```
+## running the reverse proxy
+
+Again, some assumptions here.
+
+You need to obtain SSL certifiactes, for instance from LetsEncrypt. Place the certificate and key in `proxy/certs` and/or change the volumes in the docker compose in `proxy/docker-compose.yml`.
+
+The certificate file names as well as domain names are set to the examplary value of `mydomain.com`, change them accordingly in the `proxy/conf.d/snippets/standard_ssl.conf` files to fit your domain / file names you set in the volume mount.
+
+The redmine instance is assumed to be run under the subdomain `redmine.`, you can change that in `proxy/conf.d/redmine.conf`
+
+- obtain and place SSL Certificates
+- create the Diffie–Hellman params
+    ```bash
+    openssl dhparam -out ./proxy/dhparam.pem 4096
+    ```
+- run the stack
+    ```bash
+    cd proxy
+    docker compose up -d
+    ```
+- watch the logs
+    ```bash
+    docker compose logs -f --tail=100
+    ```
+
+## the two docker files
+The Dockerfile used by default is `Dockerfile.debian.sidekiq`, there is also `Dockerfile.debian.sidekiq.oidc` which addionally integrates aplugin for OIDC/OAuth, see https://github.com/kontron/redmine_oauth/
---
+++ docker-entrypoint.sh
@@ -0,0 +1,230 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+# TODO add "-u"
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+isLikelyRedmine=
+case "$1" in
+	rails | rake ) isLikelyRedmine=1 ;;
+esac
+
+_fix_permissions() {
+	# https://www.redmine.org/projects/redmine/wiki/RedmineInstall#Step-8-File-system-permissions
+	local dirs=( config log public/plugin_assets tmp ) args=()
+	if [ "$(id -u)" = '0' ]; then
+		args+=( ${args[@]:+,} '(' '!' -user redmine -exec chown redmine:redmine '{}' + ')' )
+
+		# https://github.com/docker-library/redmine/issues/268 - scanning "files" might be *really* expensive, so we should skip it if it seems like it's "already correct"
+		local filesOwnerMode
+		filesOwnerMode="$(stat -c '%U:%a' files)"
+		if [ "$files" != 'redmine:755' ]; then
+			dirs+=( files )
+		fi
+	fi
+	# directories 755, files 644:
+	args+=( ${args[@]:+,} '(' -type d '!' -perm 755 -exec sh -c 'chmod 755 "$@" 2>/dev/null || :' -- '{}' + ')' )
+	args+=( ${args[@]:+,} '(' -type f '!' -perm 644 -exec sh -c 'chmod 644 "$@" 2>/dev/null || :' -- '{}' + ')' )
+	find "${dirs[@]}" "${args[@]}"
+}
+
+_set_redis_url() {
+	# the easiest way to configure sidekiq is by setting REDIS_URL
+	# https://github.com/sidekiq/sidekiq/wiki/Using-Redis
+	if [ -z "$REDMINE_REDIS_USERNAME" ] && [ -z "$REDMINE_REDIS_PASSWORD" ]; then
+		export REDIS_URL="redis://${REDMINE_REDIS_HOST}:${REDMINE_REDIS_PORT}/${REDMINE_REDIS_DB}"
+	elif [ -z "$REDMINE_REDIS_USERNAME" ] && [ -n "$REDMINE_REDIS_PASSWORD" ]; then
+		# redis://:the_password@host seems weird, but thats what the redis specs say...
+		export REDIS_URL="redis://:${REDMINE_REDIS_PASSWORD}@${REDMINE_REDIS_HOST}:${REDMINE_REDIS_PORT}/${REDMINE_REDIS_DB}"
+	else
+		export REDIS_URL="redis://${REDMINE_REDIS_USERNAME}:${REDMINE_REDIS_PASSWORD}@${REDMINE_REDIS_HOST}:${REDMINE_REDIS_PORT}/${REDMINE_REDIS_DB}"
+	fi
+}
+
+_configure_sql_database_connection() {
+	if [ ! -f './config/database.yml' ]; then
+		file_env 'REDMINE_DB_MYSQL'
+		file_env 'REDMINE_DB_POSTGRES'
+		file_env 'REDMINE_DB_SQLSERVER'
+
+		if [ "$MYSQL_PORT_3306_TCP" ] && [ -z "$REDMINE_DB_MYSQL" ]; then
+			export REDMINE_DB_MYSQL='mysql'
+		elif [ "$POSTGRES_PORT_5432_TCP" ] && [ -z "$REDMINE_DB_POSTGRES" ]; then
+			export REDMINE_DB_POSTGRES='postgres'
+		fi
+
+		if [ "$REDMINE_DB_MYSQL" ]; then
+			adapter='mysql2'
+			host="$REDMINE_DB_MYSQL"
+			file_env 'REDMINE_DB_PORT' '3306'
+			file_env 'REDMINE_DB_USERNAME' "${MYSQL_ENV_MYSQL_USER:-root}"
+			file_env 'REDMINE_DB_PASSWORD' "${MYSQL_ENV_MYSQL_PASSWORD:-${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-}}"
+			file_env 'REDMINE_DB_DATABASE' "${MYSQL_ENV_MYSQL_DATABASE:-${MYSQL_ENV_MYSQL_USER:-redmine}}"
+			file_env 'REDMINE_DB_ENCODING' ''
+		elif [ "$REDMINE_DB_POSTGRES" ]; then
+			adapter='postgresql'
+			host="$REDMINE_DB_POSTGRES"
+			file_env 'REDMINE_DB_PORT' '5432'
+			file_env 'REDMINE_DB_USERNAME' "${POSTGRES_ENV_POSTGRES_USER:-postgres}"
+			file_env 'REDMINE_DB_PASSWORD' "${POSTGRES_ENV_POSTGRES_PASSWORD}"
+			file_env 'REDMINE_DB_DATABASE' "${POSTGRES_ENV_POSTGRES_DB:-${REDMINE_DB_USERNAME:-}}"
+			file_env 'REDMINE_DB_ENCODING' 'utf8'
+		elif [ "$REDMINE_DB_SQLSERVER" ]; then
+			adapter='sqlserver'
+			host="$REDMINE_DB_SQLSERVER"
+			file_env 'REDMINE_DB_PORT' '1433'
+			file_env 'REDMINE_DB_USERNAME' ''
+			file_env 'REDMINE_DB_PASSWORD' ''
+			file_env 'REDMINE_DB_DATABASE' ''
+			file_env 'REDMINE_DB_ENCODING' ''
+		else
+			echo >&2
+			echo >&2 'warning: missing REDMINE_DB_MYSQL, REDMINE_DB_POSTGRES, or REDMINE_DB_SQLSERVER environment variables'
+			echo >&2
+			echo >&2 '*** Using sqlite3 as fallback. ***'
+			echo >&2
+
+			adapter='sqlite3'
+			host='localhost'
+			file_env 'REDMINE_DB_PORT' ''
+			file_env 'REDMINE_DB_USERNAME' 'redmine'
+			file_env 'REDMINE_DB_PASSWORD' ''
+			file_env 'REDMINE_DB_DATABASE' 'sqlite/redmine.db'
+			file_env 'REDMINE_DB_ENCODING' 'utf8'
+
+			mkdir -p "$(dirname "$REDMINE_DB_DATABASE")"
+			if [ "$(id -u)" = '0' ]; then
+				find "$(dirname "$REDMINE_DB_DATABASE")" \! -user redmine -exec chown redmine '{}' +
+			fi
+		fi
+
+		REDMINE_DB_ADAPTER="$adapter"
+		REDMINE_DB_HOST="$host"
+		echo "$RAILS_ENV:" > config/database.yml
+		for var in \
+			adapter \
+			host \
+			port \
+			username \
+			password \
+			database \
+			encoding \
+		; do
+			env="REDMINE_DB_${var^^}"
+			val="${!env}"
+			[ -n "$val" ] || continue
+			echo "  $var: \"$val\"" >> config/database.yml
+		done
+	fi
+}
+
+_configure_redis_connection() {
+	# there doesnt seem to be a way to check if this config was already done, is there?
+	# the sidekiq config is in additional_environment.rb, but here are many other things which could create that
+	# i think all thats left to do here is check the env vars
+	# this will be ugly...
+	if [ -n "$REDMINE_REDIS_HOST" ] \
+		|| [ -n "$REDMINE_REDIS_PORT" ] \
+		|| [ -n "$REDMINE_REDIS_DB" ] \
+		|| [ -n "$REDMINE_REDIS_USERNAME" ] \
+		|| [ -n "$REDMINE_REDIS_PASSWORD" ] \
+		|| [ -n "$REDMINE_REDIS_PASSWORD_FILE" ];
+	then
+		# go for default redis or valkey?
+		file_env 'REDMINE_REDIS_HOST' 'valkey'
+		file_env 'REDMINE_REDIS_PORT' '6379'
+		file_env 'REDMINE_REDIS_DB' '0'
+
+		file_env 'REDMINE_REDIS_USERNAME' ''
+		file_env 'REDMINE_REDIS_PASSWORD' ''
+		# redis protocol allows empty usernames and a set password
+		# but not the other way around, if a username is set, you must also have a password
+		if [ -n "$REDMINE_REDIS_USERNAME" ] && [ -z "$REDMINE_REDIS_PASSWORD" ]; then
+			echo >&2
+			echo >&2 'warning: REDMINE_REDIS_USERNAME environment variable is set, but not REDMINE_REDIS_PASSWORD'
+			echo >&2 'warning: skipping sidekiq config'
+			echo >&2
+			break 2
+		fi
+		
+		_set_redis_url
+
+		# seems like ./config/additional_environment.rb always gets created by the docker file
+		# but its owned by root...
+		ac_file_path="./config/additional_environment.rb"
+		# just in case the user deleted that file, we should check for it anyways
+		if [ ! -f "$ac_file_path" ] || ! grep -Fxq "config.active_job.queue_adapter = :sidekiq" "$ac_file_path"; then
+			echo "config.active_job.queue_adapter = :sidekiq" >> "$ac_file_path"
+		fi
+
+	fi
+}
+
+_handle_secrets() {
+	if [ ! -s config/secrets.yml ]; then
+		file_env 'REDMINE_SECRET_KEY_BASE'
+		if [ -n "$REDMINE_SECRET_KEY_BASE" ]; then
+			cat > 'config/secrets.yml' <<-YML
+				$RAILS_ENV:
+				  secret_key_base: "$REDMINE_SECRET_KEY_BASE"
+			YML
+		elif [ ! -f config/initializers/secret_token.rb ]; then
+			rake generate_secret_token
+		fi
+	fi
+}
+
+# allow the container to be started with `--user`
+if [ -n "$isLikelyRedmine" ] && [ "$(id -u)" = '0' ]; then
+	_fix_permissions
+	exec gosu redmine "$BASH_SOURCE" "$@"
+fi
+
+if [ -n "$isLikelyRedmine" ] || [ "$*" == "bundle exec sidekiq" ]; then
+	_fix_permissions
+
+	_configure_sql_database_connection
+
+	_configure_redis_connection
+
+	# install additional gems for Gemfile.local and plugins
+	bundle check || bundle install
+
+	_handle_secrets
+
+fi
+
+if [ -n "$isLikelyRedmine" ]; then
+	if [ "$1" != 'rake' -a -z "$REDMINE_NO_DB_MIGRATE" ]; then
+		rake db:migrate
+	fi
+
+	if [ "$1" != 'rake' -a -n "$REDMINE_PLUGINS_MIGRATE" ]; then
+		rake redmine:plugins:migrate
+	fi
+
+	# remove PID file to enable restarting the container
+	rm -f tmp/pids/server.pid
+fi
+
+exec "$@"
